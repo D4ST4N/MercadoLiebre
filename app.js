ce8ecce4b685e45ac55ba2944a757c079740d5f8
Gaston Nicolás Díaz <gastondiazn@gmail.com>
2023-11-13 08:39:04 -0300

    form

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/ofertas',(req,res)=>res.sendFile(path.join(__dirname,"views", "ofertas
 app.get('/tiendas',(req,res)=>res.sendFile(path.join(__dirname,"views", "tiendas.html")))
 app.get('/vender',(req,res)=>res.sendFile(path.join(__dirname,"views", "vender.html")))
 app.get('/ayuda',(req,res)=>res.sendFile(path.join(__dirname,"views", "ayuda.html")))
-app.get('/creaTuCuenta',(req,res)=>res.sendFile(path.join(__dirname,"views", "creaTuCuenta.html")))
-app.get('/ingresa',(req,res)=>res.sendFile(path.join(__dirname,"views", "ingresa.html")))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,"views", "register.html")))
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,"views", "login.html")))
 app.get('/MisCompras',(req,res)=>res.sendFile(path.join(__dirname,"views", "MisCompras.html")))
 app.listen(PORT,()=>{
     console.log(`Server Running in http://localhost:${PORT}`);
